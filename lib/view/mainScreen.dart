6d012c482671fa8e3f808b944ba04abed2044082
thisi <thisiabhimaurya@gmail.com>
2024-02-25 06:12:38 +0530

    1st commit

--- lib/view/mainScreen.dart
+++ lib/view/mainScreen.dart
@@ -63,7 +63,7 @@ class _MainScreenState extends State<MainScreen> {
             style: TextStyle(fontSize: 30, fontWeight: FontWeight.w800),
           ),
           SizedBox(
-            height: 10,  //height
+            height: 10,  //heightnxj
           ),
           Text(
             "Target ${targetMeme} Memes",
